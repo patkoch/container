7aa5a18bfbc7cc6e7b4d8a63cfdf31a8edbfa3ff
Patrick Koch <Patrick.Koch@avl.com>
2023-03-14 19:36:39 +0100

    Created two simple Linux Dockerfiles for AKS Demo

---
+++ azure-meetup-demo/linux-job/scripts/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Hi, I'm a Linux Container, I'll terminate in 2 minutes"
+
+sleep 2m
+
+exit
---
+++ azure-meetup-demo/linux-statefulset/scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Hi, I'm a Linux Container, I'll not terminate!"
+
+while :
+do
+	echo Still running
+	sleep 2
+done
+
+exit
